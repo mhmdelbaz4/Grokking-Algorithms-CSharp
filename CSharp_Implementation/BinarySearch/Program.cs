78f231a1cce6e7cd7bac699a06382896a21eb5ae
Mohamed Elbaz <mhmdelbaz57@gmail.com>
2023-03-17 10:23:02 +0200

    binary search v2

--- CSharp_Implementation/BinarySearch/Program.cs
+++ CSharp_Implementation/BinarySearch/Program.cs
@@ -6,20 +6,18 @@
         {
             int[] arr = new int[] { 1, 3, 5, 7, 9 };
 
-            int position_5 = Algorithms.BinarySearch(arr, 5);
+            int position_5 = BinarySearch(arr, 5);
             Console.WriteLine($"index of 5 in arr = {position_5}"); //2
 
-            int position_1 = Algorithms.BinarySearch(arr, 1);
+            int position_1 = BinarySearch(arr, 1);
             Console.WriteLine($"index of 1 in arr = {position_1}"); //0
 
-            int position_10 = Algorithms.BinarySearch(arr, 10);
+            int position_10 = BinarySearch(arr, 10);
             Console.WriteLine($"index of 1 in arr = {position_10}"); //-1
 
         }
-    }
 
-    static class Algorithms
-    {
+
         //-1 represents not found in the array
         public static int BinarySearch(int[] seuence, int item)
         {
